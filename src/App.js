e07ce4500086a891bf5fb2de417f0fdc50344e00
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-09-06 13:56:18 +0300

    create-a-task-list-using-a-form

--- src/App.js
+++ src/App.js
@@ -4,45 +4,44 @@ import TaskList from './components/TaskList';
 import { useState } from 'react';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const createTask = (title, taskDescription) => {
-    const createdTasks = [
-      ...tasks,
-      {
-        id: Math.round(Math.random() * 999999),
-        title,
-        taskDescription,
-      },
-    ];
-    setTasks(createdTasks);
-  };
-  const deleteTaskById = (id) => {
-    const afterDeletingTasks = tasks.filter((task) => {
-      return task.id !== id;
-    });
-    setTasks(afterDeletingTasks);
-  };
-  const editTaskById = (id, updatedTitle, updatedTaskDescription) => {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === id) {
-        return { id, title: updatedTitle, taskDescription: updatedTaskDescription };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
-  };
+    const [tasks, setTasks] = useState([]);
+    const createTask = (title, taskDescription) => {
+        const createdTasks = [
+          ...tasks,
+          {
+              id: Math.round(Math.random() * 999999),
+              title,
+              taskDescription,
+          },
+        ];
+        setTasks(createdTasks);
+    };
+    const deleteTaskById = (id) => {
+        const afterDeletingTasks = tasks.filter((task) => {
+            return task.id !== id;
+        });
+        setTasks(afterDeletingTasks);
+    };
+    const editTaskById = (id, updatedTitle, updatedTaskDescription) => {
+        const updatedTasks = tasks.map((task) => {
+            if (task.id === id) {
+                return { id, title: updatedTitle, taskDescription: updatedTaskDescription };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    };
 
-  return (
-    <div className="App">
-      <TaskCreate onCreate={createTask} />
-      <h1>Your Tasks</h1>
-      <TaskList
-        tasks={tasks}
-        onDelete={deleteTaskById}
-        onUpdate={editTaskById}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <TaskCreate onCreate={createTask} />
+            <h1>Your Tasks</h1>
+            <TaskList tasks={tasks}
+                      onDelete={deleteTaskById}
+                      onUpdate={editTaskById}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/TaskShow.js
+++ src/components/TaskShow.js
@@ -2,41 +2,41 @@ import { useState } from 'react';
 import TaskCreate from './TaskCreate';
 
 function TaskShow({ task, onDelete, onUpdate }) {
-  const [showEdit, setShowEdit] = useState(false);
-  const handleDeleteClick = () => {
-    onDelete(task.id);
-  };
-  const handleEditClick = () => {
-    setShowEdit(!showEdit);
-  };
-  const handleSubmit = (id, updatedTitle, updatedTaskDescription) => {
-    setShowEdit(false);
-    onUpdate(id, updatedTitle, updatedTaskDescription);
-  };
+    const [showEdit, setShowEdit] = useState(false);
+    const handleDeleteClick = () => {
+        onDelete(task.id);
+    };
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+    const handleSubmit = (id, updatedTitle, updatedTaskDescription) => {
+        setShowEdit(false);
+        onUpdate(id, updatedTitle, updatedTaskDescription);
+    };
 
-  console.log(task);
-  return (
-    <div className="task-show">
-      {showEdit ? (
-        <TaskCreate task={task} taskformUpdate={true} onUpdate={handleSubmit} />
-      ) : (
-        <div>
-          <h3 className="task-title">Your Task</h3>
-          <p>{task.title}</p>
-          <h3 className="task-title">To Do</h3>
-          <p>{task.taskDescription}</p>
-          <div>
-            <button className="task-delete" onClick={handleDeleteClick}>
-              Delete
-            </button>
-            <button className="task-edit" onClick={handleEditClick}>
-              Update
-            </button>
-          </div>
+    console.log(task);
+    return (
+        <div className="task-show">
+            {showEdit ? (
+                <TaskCreate task={task} taskformUpdate={true} onUpdate={handleSubmit} />
+            ) : (
+                <div>
+                    <h3 className="task-title">Your Task</h3>
+                    <p>{task.title}</p>
+                    <h3 className="task-title">To Do</h3>
+                    <p>{task.taskDescription}</p>
+                    <div>
+                        <button className="task-delete" onClick={handleDeleteClick}>
+                            Delete
+                        </button>
+                        <button className="task-edit" onClick={handleEditClick}>
+                            Update
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default TaskShow;
